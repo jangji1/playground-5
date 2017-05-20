9debc1bccd55049df711c188f1d2d3dbd7a27e81
jangji1 <jangji1@gmail.com>
2017-05-20 12:00:00 +0900

    투두앱 추가/삭제

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import Header from './Header';
+import TodoList from './TodoList';
+import Footer from './Footer';
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            todos: [{
+                id: 1000,
+                text: '치킨에 맥주',
+                isDone: false
+            }, {
+                id: 1001,
+                text: '삼겹살에 소주',
+                isDone: false
+            }, {
+                id: 1002,
+                text: '떡튀순',
+                isDone: true
+            }]
+        };
+    }
+
+    addTodo = text => {
+        this.setState({
+            todos: [ ...this.state.todos, {
+                id: Data.now(),
+                text,
+                isDone: false
+            }]
+        });
+    }
+
+    deleteTodo = id => {
+        const newTodos = [...this.state.todos];
+        const deleteIndex = newTodos.findIndex(v => v.id ===id);
+        newTodos.splice(deleteIndex, 1);
+        this.setState({
+            todos: newTodos
+        })
+    }
+
+    render() {
+        return (
+            <div className="todo-app">
+                <Header
+                    addTodo={this.addTodo}
+                />
+                <TodoList
+                    todos={this.state.todos}
+                    deleteTodo={this.deleteTodo}
+                />
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/Todo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Todo extends React.Component {
+    render() {
+        return (
+            <li className="todo-item">
+                <button className="toggle"/>
+                <div className="todo-item__view">
+                    <div className="todo-item__view__text"></div>
+                    <button 
+                        className="todo-item__destroy"
+                        onClick={this.props.deleteTodo}
+                    ></button>
+                </div>
+                <input
+                    type="text"
+                    className="todo-item__edit"
+                />
+            </li>
+        );
+    }
+}
+
+export default Todo;
