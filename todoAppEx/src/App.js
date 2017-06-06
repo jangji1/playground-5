19b592c95c36cd9edf38a3c9226a09d93fbad5a3
gilbert-go <gilbert.go@kakaocorp.com>
2017-06-06 20:04:59 +0900

    ajax add

--- todoAppEx/src/App.js
+++ todoAppEx/src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Header from './Header';
 import TodoList from './TodoList';
 import Footer from './Footer';
+import axios from 'axios';
+
+const ax = axios.create({
+    baseURL : 'http://localhost:2403/todos'
+});
 
 //형제간의 데이터 통신이필요할때 부모로 올려서 처리
 //this.state.todos.push -> setState를 거치지않고 임의의 변경은 절대로 해서는 안된다.
@@ -10,24 +15,18 @@ import Footer from './Footer';
 //addTodo 자기자신이 호출한게아니라 Header에서 호출이되서 올라와서 this.props가 this가 되어버려서 this 바인딩이 필요
 
 class App extends React.Component {
+    // render는 무조건 2 번실행된다 ... 비동기기 떄문에 ..
+    componentWillMount(){
+        ax.get('/')
+        .then(res => {
+            this.setState({
+                todos : res.data
+            })
+        });
+    }
+
     state = {
-        todos: [
-            {
-                text: '배고파',
-                isDone: false,
-                id: 1000
-            },
-            {
-                text: '밥줘',
-                isDone: false,
-                id: 1001
-            },
-            {
-                text: '졸려',
-                isDone: true,
-                id: 1002
-            }
-        ],
+        todos: [],
         editingId: null,
         filterName: 'All'
     };
@@ -39,21 +38,24 @@ class App extends React.Component {
     };
 
     addTodo = text => {
-        this.setState({
-            todos: [...this.state.todos, {
-                text: text,
-                isDone: false,
-                id: Date.now()
-            }]
-        });
+        ax.post('/', {text})
+            .then(res => {
+                this.setState({
+                    todos: [...this.state.todos, res.data]
+                });
+            })
     };
     deleteTodo = id => {
         const newTodos = [...this.state.todos];
         const deleteIndex = newTodos.findIndex(v => v.id === id);
-        newTodos.splice(deleteIndex, 1);
-        this.setState({
-            todos: newTodos
-        })
+
+        ax.delete(`/${id}`)
+            .then(() => {
+                newTodos.splice(deleteIndex, 1);
+                this.setState({
+                    todos: newTodos
+                })
+            });
     };
     editTodo = id => { // 수정
         this.setState({
@@ -63,13 +65,14 @@ class App extends React.Component {
     saveTodo = (id, newText) => {
         const newTodos = [...this.state.todos];
         const editIndex = newTodos.findIndex(v => v.id === id);
-        newTodos[editIndex] = Object.assign({}, newTodos[editIndex], {
-            text: newText
-        });
-        this.setState({
-            todos: newTodos,
-            editingId: null
-        })
+        ax.put(`/${id}`, {text : newText})
+            .then(res => {
+                newTodos[editIndex] = res.data;
+                this.setState({
+                    todos: newTodos,
+                    editingId: null
+                })
+            });
     };
     cancelEdit = () => {
         this.setState({
@@ -79,28 +82,38 @@ class App extends React.Component {
     togleTodo = id => {
         const newTodos = [...this.state.todos];
         const editIndex = newTodos.findIndex(v => v.id === id);
-        newTodos[editIndex] = Object.assign({}, newTodos[editIndex], {
-            isDone: !newTodos[editIndex].isDone
-        });
-        this.setState({
-            todos: newTodos,
-        })
+        ax.put(`/${id}`,{isDone : !newTodos[editIndex].isDone})
+            .then(res => {
+                newTodos[editIndex] = res.data;
+                this.setState({
+                    todos: newTodos,
+                })
+            });
     };
     toggleAll = () => { // every , some 전부순회하느냐 하나만 되도 하느냐
         const newDone = this.state.todos.some(v => !v.isDone);
-        const newTodos = this.state.todos.map(v => Object.assign({}, v, {
-                isDone: newDone
-            })
-        );
-        this.setState({
-            todos: newTodos
-        });
+        const axArray = this.state.todos.map(v => (
+            ax.put(`/${v.id}`, {isDone : newDone})
+        ));
+
+        axios.all(axArray)
+            .then(res => {
+                this.setState({
+                    todos: res.map(v => v.data)
+                });
+            });
     };
-    clearCompleted = () => {
+    clearCompleted = () => { // 완료 된애들 , 완료안된애들 db 삭제때문에 나눔
         const newTodos = this.state.todos.filter(v => !v.isDone);
-        this.setState({
-            todos: newTodos
-        })
+        const axArray = this.state.todos.filter(v => v.isDone)
+            .map(v => {
+                ax.delete(`/${v.id}`)
+            });
+        axios.all(axArray).then(() => {
+            this.setState({
+                todos: newTodos
+            })
+        });
     };
 
     render() {
@@ -110,7 +123,7 @@ class App extends React.Component {
             filterName
         }= this.state;
         const activeLength = todos.filter(v => !v.isDone).length;
-        const hasCompleted = todos.findIndex(v => v.isDone) > 0;
+        const hasCompleted = todos.findIndex(v => v.isDone) >=  0;
         const filteredTodos = filterName === 'All' ? todos : todos.filter(v => (
                 (filterName === 'Active' && !v.isDone) ||
                 (filterName === 'Completed' && v.isDone)
