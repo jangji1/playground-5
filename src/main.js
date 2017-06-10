fe51b230181b8f929f2ada9cfdcec8a919cebe53
isangcheol <lsc900427@gmail.com>
2017-06-10 12:27:07 +0900

    redux

--- src/main.js
+++ src/main.js
@@ -1,21 +1,32 @@
+// import React from 'react';
+// import ReactDom from 'react-dom';
+// import {
+//     BrowserRouter as Router,
+//     Route,
+//     Switch
+// } from 'react-router-dom';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+//
+// ReactDom.render(
+//     <Router>
+//         <Switch>
+//             <Route exact path ="/" component={App}/>
+//             <Route path ="/:filterName" component={App}/>
+//         </Switch>
+//     </Router>,
+//     document.getElementById('root')
+//
+//
+// );
+
+
 import React from 'react';
 import ReactDom from 'react-dom';
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch
-} from 'react-router-dom';
-import ReactDOM from 'react-dom';
-import App from './App';
+import App from './reduxPractice/App';
 
 ReactDom.render(
-    <Router>
-        <Switch>
-            <Route exact path ="/" component={App}/>
-            <Route path ="/:filterName" component={App}/>
-        </Switch>
-    </Router>,
+    <App/>,
     document.getElementById('root')
 
-
-);
+)
