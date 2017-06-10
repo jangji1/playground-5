99a38e0cba4f4c704b87c36d97b7e5d1155aaadf
gilbert-go <gilbert.go@kakaocorp.com>
2017-06-10 11:11:18 +0900

    filter

---
+++ todoAppEx/src/routerPractice/Components.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export const Home = () => (
+  <h2>Home</h2>
+);
+export const About = ({children}) => (
+  <div>
+    <h2>About</h2>
+    {children?<div>{children}</div>:null}
+  </div>
+
+);
+export const Name = () => (
+  <h2>Name</h2>
+);
+const portfolioList = [
+  {id : 0, text : 'portfolio #0'},
+  {id : 1, text : 'portfolio #1'},
+  {id : 2, text : 'portfolio #2'}
+];
+export const Portfolio = ({match}) => {
+  const filteredList = (match.params && match.params.id) ?
+    portfolioList.filter(v => v.id == match.params.id) :
+    portfolioList;
+  const renderList = filteredList.map(v => (
+    <li key={v.id}>{v.text}</li>
+  ));
+  return (
+    <div>
+      <h2>Portfolio</h2>
+      <ul>{renderList}</ul>
+    </div>
+  )
+};
